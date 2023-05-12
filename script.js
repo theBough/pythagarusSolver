c167f84c9fe4cc68889897f113244b97871295b3
corbin Boughen <50217007+theBough@users.noreply.github.com>
2023-05-12 10:06:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,18 @@ var a = document.getElementById("a")
 var b = document.getElementById("b")
 var c = document.getElementById("c")
 
-function solve(){
-  /*
-  this function will solve for either a,b or c
-  */
+function solveC(){
   var aNum = parseInt(a.value)
   var bNum = parseInt(b.value)
   c.value = Math.sqrt((aNum*aNum) + (bNum*bNum))
+}//end function
+function solveA(){
+  var cNum = parseInt(c.value)
+  var bNum = parseInt(b.value)
+  a.value = Math.sqrt((cNum*cNum) - (bNum*bNum))
+}//end function
+function solveB(){
+  var cNum = parseInt(c.value)
+  var aNum = parseInt(a.value)
+  b.value = Math.sqrt((cNum*cNum) - (aNum*aNum))
 }//end function
